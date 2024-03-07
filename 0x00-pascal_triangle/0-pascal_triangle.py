00dfc75ac19b0f348deaabbd6bd092ef39a31b5e
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-07 13:31:18 +0200

    Update 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    pascal = []
+
+    for i_row in range(n):
+        row = []
+
+        for j_col in range(i_row + 1):
+            if j_col == 0 or j_col == i_row:
+                row.append(1)
+            else:
+                column_val = pascal[i_row - 1][j_col] + pascal[i_row - 1][j_col - 1]
+                row.append(column_val)
+        pascal.append(row)
+    return pascal
