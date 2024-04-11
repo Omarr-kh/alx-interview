4c13cc4d90cc808765133149ea3d9148227ff6c0
Omarr-kh <omar1752003@gmail.com>
2024-04-11 16:15:43 +0000

    formatted the output messages

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -54,17 +54,17 @@ def print_solutions(solutions):
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
-        print("Usage: nqueens n")
+        print("Usage: nqueens N")
         sys.exit(1)
 
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print("n must be a number")
+        print("N must be a number")
         sys.exit(1)
 
     if n < 4:
-        print("n must be at least 4")
+        print("N must be at least 4")
         sys.exit(1)
 
     solutions = solveNQueens(n)
