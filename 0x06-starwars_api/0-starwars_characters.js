db2cc9dfe78bca33762a0d4a8c9258607e285e33
Omarr-kh <omar1752003@gmail.com>
2024-04-19 11:12:35 +0000

    task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const api_url = 'https://swapi-api.alx-tools.com/api';
+
+if (process.argv.length > 2) {
+  request(`${api_url}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersList = JSON.parse(body).characters;
+    const characterNames = charactersList.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (reqErr, _, reqBody) => {
+          if (reqErr) {
+            reject(reqErr);
+	  }
+          resolve(JSON.parse(reqBody).name);
+	});
+      }));
+    Promise.all(characterNames)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
