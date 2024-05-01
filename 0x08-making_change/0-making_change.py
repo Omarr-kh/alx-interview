5edde5ae41b93ce5ef2499a43b9fff0d0e362504
Omarr-kh <omar1752003@gmail.com>
2024-05-01 17:16:30 +0000

    greedy sol

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" Solution to make change """
+""" DP algorithm to solve Make Change """
 
 
 def makeChange(coins, total):
-    """ greedy solution
-    """
+    """ the algorithm to solve the make change problem """
     if total <= 0:
         return 0
-    rest = total
-    total_coins = 0
-    idx = 0
-    coins_sorted = sorted(coins, reverse=True)
+
+    coins = sorted(coins, reverse=True)
+    i = 0
+    coins_count = 0
     n = len(coins)
-    while rest > 0:
-        if idx >= n:
+    while total > 0:
+        if i >= n:
             return -1
-        if rest - coins_sorted[idx] >= 0:
-            rest -= coins_sorted[idx]
-            total_coins += 1
+        if total - coins[i] >= 0:
+            total -= coins[i]
+            coins_count += 1
         else:
-            idx += 1
-    return total_coins
+            i += 1
+    return coins_count
