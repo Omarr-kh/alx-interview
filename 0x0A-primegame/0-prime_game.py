3db6942207411f8acf92eb249aa0370038b7e91d
Omar <omarkhaled1752003@yahoo.com>
2024-05-18 14:44:32 +0300

    pep8

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Solution for Prime Game Problem """
+"""Solution for Prime Game Problem."""
 
 
 def isWinner(x, nums):
-    """ Determine winner after x rounds """
+    """Determine winner after x rounds."""
     if x < 1 or not nums:
         return None
 
@@ -16,17 +16,18 @@ def isWinner(x, nums):
 
     # Sieve of Eratosthenes to get primes
     for i, is_prime in enumerate(primes_list, 1):
-        if i == 1 or is_prime == False:
+        if i == 1 or not is_prime:
             continue
         for k in range(i + i, max_n + 1, i):
             primes_list[k - 1] = False
 
-    # loop through rounds and check n
+    # Loop through rounds and check n
     for _, m in zip(range(x), nums):
         round_n_primes = len(list(filter(lambda z: z, primes_list[0:m])))
         maria_wins += round_n_primes % 2 == 1
         ben_wins += round_n_primes % 2 == 0
-    # can't determine winner
+
+    # Can't determine winner
     if ben_wins == maria_wins:
         return None
     return 'Ben' if ben_wins > maria_wins else 'Maria'
