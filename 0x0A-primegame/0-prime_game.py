76e6155293333dd27d3b24c06de84be0c9bf3fe3
Omar <omarkhaled1752003@yahoo.com>
2024-05-18 14:27:37 +0300

    prime game solution

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Solution for Prime Game Problem """
+
+
+def isWinner(x, nums):
+    """ Determine winner after x rounds """
+    if x < 1 or not nums:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    max_n = max(nums)
+    primes_list = [True for _ in range(max_n + 1)]
+    primes_list[0] = False
+
+    # Sieve of Eratosthenes to get primes
+    for i, is_prime in enumerate(primes_list, 1):
+        if i == 1 or is_prime == False:
+            continue
+        for k in range(i + i, max_n + 1, i):
+            primes_list[k - 1] = False
+
+    # loop through rounds and check n
+    for _, round_n in zip(range(x), nums):
+        round_n_primes = len(list(filter(lambda num: num, primes_list[0:round_n])))
+        maria_wins += round_n_primes % 2 == 1
+        ben_wins += round_n_primes % 2 == 0
+    # can't determine winner
+    if ben_wins == maria_wins:
+        return None
+    return 'Ben' if ben_wins > maria_wins else 'Maria'
